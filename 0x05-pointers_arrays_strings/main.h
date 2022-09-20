17312c46716f0b0825b267429fc7a5cbe2d191a2
Cynthia Abba <abbaadaeze@gmail.com>
2022-09-19 19:41:57 -0700

    task 0

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -22,9 +22,9 @@ void reset_to_98(int *n);
  * @b: second integer
 */
 
-void swap_int(int *a, int *b)
-int _strlen(char *s)
-void _puts(char *str)
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
