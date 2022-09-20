5f373eefc03b1317734a050e7bc3a6e6ca65748d
Cynthia Abba <abbaadaeze@gmail.com>
2022-09-19 19:36:06 -0700

    task 0

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,7 +8,20 @@
  */
 
 int _putchar(char c);
+
+/**
+ * reset_to_98 - resets variable to 98
+ * @n: variable with pointer
+ */
+
 void reset_to_98(int *n);
+
+/**
+ * swap_int - swaps integer values
+ * @a: first integer
+ * @b: second integer
+*/
+
 void swap_int(int *a, int *b)
 int _strlen(char *s)
 void _puts(char *str)
